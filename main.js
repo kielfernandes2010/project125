3bb652df1e12197c4519f726bdc9832cfbbecef6
kielfernandes2010 <74675067+kielfernandes2010@users.noreply.github.com>
2021-08-02 20:11:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    
+
+    canvas=createCanvas(550,550);
+    canvas.position(560, 150);
+    canvas.center();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#FFA500');
+    
+
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized!')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+
+
